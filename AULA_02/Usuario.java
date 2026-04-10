bbe3a88a3e601eb7076142e8b08456e8224bd355
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-04-09 21:09:18 -0300

    [feat] - Cria construtor da classe

--- AULA_02/Usuario.java
+++ AULA_02/Usuario.java
@@ -8,4 +8,15 @@ public class Usuario {
     private String nome;
     private String email;
 
+    //2. Contrutor vazio  (necessário para Hinernate/JPA)
+    public Usuario(){
+    }
+
+    // 3. Construtor com parâmetros
+    public Usuario(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
 }
