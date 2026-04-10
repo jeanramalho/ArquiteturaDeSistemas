06b0b060d5597316312bd7d99edc57e9b66681ac
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-04-09 21:06:37 -0300

    [feat] - Cria classe java para Model de usuário

---
+++ AULA_02/Usuario.java
@@ -0,0 +1,11 @@
+package AULA_02;
+
+// Pode ser necessário implementar Serializable para objetos complexos
+public class Usuario {
+
+    // 1. Atributos Privados
+    private Long id;
+    private String nome;
+    private String email;
+
+}
