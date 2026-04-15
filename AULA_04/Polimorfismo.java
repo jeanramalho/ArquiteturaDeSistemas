ea7c015f3bd42f66f23ff787bb735de6b93d535f
jeanramalho.dev <jeanramalho.dev@gmail.com>
2026-04-15 20:44:16 -0300

    [feat] - Cria classe com herança do produto alimenticio e realiza polimorfismo reescrevendo o método exibirResumo

--- AULA_04/Polimorfismo.java
+++ AULA_04/Polimorfismo.java
@@ -33,12 +33,32 @@ class Produto {
         return "Produto: " + nome + ", Categoria: " + categoria + ", Preço: R$" + preco + ", Quantidade: " + quantidade;
     }
 }
+
+class ProdutoAlimenticio extends Produto {
+    private String dataValidade;
+    
+    public ProdutoAlimenticio(String nome, String categoria, double preco, int quantidade, String dataValidade) {
+        super(nome, categoria, preco, quantidade);
+        this.dataValidade = dataValidade;
+    }
+
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    @Override
+    public String exibirResumo() {
+        return super.exibirResumo() + ", Data de Validade: " + dataValidade;
+    }
+}
+
 public class Polimorfismo {
 
     public static void main(String[] args) {
         Produto p1 = new Produto("Notebook", "Eletrônicos", 3500.00, 10);
         Produto p2 = new Produto("Camiseta", "Roupas", 50.00, 100);
         Produto p3 = new Produto("Cadeira", "Móveis", 200.00, 20);
+        ProdutoAlimenticio p4 = new ProdutoAlimenticio("Leite", "Alimentos", 5.00, 50, "2024-12-31");
 
         System.out.println(p1.exibirResumo());
         System.out.println(p2.exibirResumo());
